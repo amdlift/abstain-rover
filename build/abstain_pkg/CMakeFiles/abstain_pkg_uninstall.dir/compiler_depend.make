1263a8681bbfab09b0b1ee26c661374f01efe8ca
Aaron Davis <aaron.m.davis@comcast.net>
2025-10-21 14:22:11 -0500

    removed examples

---
+++ build/abstain_pkg/CMakeFiles/abstain_pkg_uninstall.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty custom commands generated dependencies file for abstain_pkg_uninstall.
+# This may be replaced when dependencies are built.
---
+++ build/abstain_pkg/CMakeFiles/gs_node.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gs_node.dir/src/gs_node.cpp.o"
+  "CMakeFiles/gs_node.dir/src/gs_node.cpp.o.d"
+  "gs_node"
+  "gs_node.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gs_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
